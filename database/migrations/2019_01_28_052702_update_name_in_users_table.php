cb12bc3cf047558c30b2ab85a60a8f2b185caae5
Remarc Balisi <=>
2019-01-28 13:32:03 +0800

    add dbal and add fname, mname and lname in users table

---
+++ database/migrations/2019_01_28_052702_update_name_in_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateNameInUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'fname');
+            $table->string('mname');
+            $table->string('lname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
