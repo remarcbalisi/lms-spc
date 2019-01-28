27e57fbc5162190d3bdf9b2267037a2189be7e39
Remarc Balisi <=>
2019-01-28 13:39:47 +0800

    create privilege table

---
+++ database/migrations/2019_01_28_053814_create_privilege_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrivilegeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('privilege', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('action', 30);
+            $table->string('table', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('privilege');
+    }
+}
