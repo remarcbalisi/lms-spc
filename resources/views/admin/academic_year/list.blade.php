d7bba309834601a58d2927e31cba5e5687989a8b
Remarc Balisi <remarc.balisi@gmail.com>
2019-01-31 07:14:21 +0800

    academic year list: ok

---
+++ resources/views/admin/academic_year/list.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Academic Year</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Start</th>
+                                    <th scope="col">End</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($academic_years as $academic_year)
+                                    <tr>
+                                        <th scope="row">{{$academic_year->id}}</th>
+                                        <td>{{$academic_year->start}}</td>
+                                        <td>{{$academic_year->end}}</td>
+                                        <td>
+                                            <a>
+                                                <button type="button" class="btn btn-primary">View</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -44,10 +44,9 @@
                                 <a class="dropdown-item" href="{{ route('admin-academic-year-create') }}">
                                     Create
                                 </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
+                                <a class="dropdown-item" href="{{ route('admin-academic-year-list') }}">
+                                    List
+                                </a>
                             </div>
                         </li>
                     @endauth
