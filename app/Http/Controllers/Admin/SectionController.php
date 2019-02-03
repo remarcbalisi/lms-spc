a8fc83013b20c03bd48aef354277c6ade788f2da
Remarc Balisi <remarc.balisi@gmail.com>
2019-02-04 06:45:57 +0800

    creating of section with course ok

--- app/Http/Controllers/Admin/SectionController.php
+++ app/Http/Controllers/Admin/SectionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Course;
 use App\Section;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,17 +10,27 @@ use App\Http\Controllers\Controller;
 class SectionController extends Controller
 {
     public function create(){
-        return view('admin.section.create');
+        return view('admin.section.create')->with([
+            "courses" => Course::get(),
+        ]);
     }
 
     public function store(Request $request){
         $request->validate([
             'name' => 'required|unique:section|max:45',
+            'course_id' => 'required'
         ]);
+
+        if( $section = Section::where(['name' => $request->input('name'), 'course_id' => $request->input('course_id')])->first() ){
+            return redirect()->back()->withErrors([
+                'exist' => "Section " . $section->name . " Already exist"
+            ]);
+        }
 
         $new_section = new Section;
         $new_section->name = $request->input('name');
         $new_section->slug = strtolower(str_replace(" ", "-", $request->input('name')));
+        $new_section->course_id = $request->input('course_id');
         $new_section->save();
 
         return redirect()->back()->with([
--- app/Section.php
+++ app/Section.php
@@ -9,6 +9,11 @@ class Section extends Model
     protected $table = "section";
 
     protected $fillable = [
-        "name", "slug",
+        "name", "slug", "course_id"
     ];
+
+    public function course(){
+        $this->belongsTo("App\Course", "course_id");
+    }
+
 }
