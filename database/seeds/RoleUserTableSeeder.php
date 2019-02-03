62f98f90bc08f3feae225c790cfe5d3f162c9b1b
Remarc Balisi <remarc.balisi@gmail.com>
2019-02-03 08:33:41 +0800

    add seeds for admin2, leacturer, learner

--- database/seeds/RoleUserTableSeeder.php
+++ database/seeds/RoleUserTableSeeder.php
@@ -15,5 +15,17 @@ class RoleUserTableSeeder extends Seeder
             'user_id' => 1,
             'role_id' => 1,
         ]);
+        DB::table('role_user')->insert([
+            'user_id' => 2,
+            'role_id' => 1,
+        ]);
+        DB::table('role_user')->insert([
+            'user_id' => 3,
+            'role_id' => 2,
+        ]);
+        DB::table('role_user')->insert([
+            'user_id' => 4,
+            'role_id' => 3,
+        ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,5 +18,29 @@ class UsersTableSeeder extends Seeder
             'email' => 'remarc.balisi@gmail.com',
             'password' => bcrypt('admin'),
         ]);
+
+        DB::table('users')->insert([
+            'fname' => 'Admin 2',
+            'mname' => 'Admin Middle name 2',
+            'lname' => 'Admin Last name 2',
+            'email' => 'admin2@gmail.com',
+            'password' => bcrypt('admin'),
+        ]);
+
+        DB::table('users')->insert([
+            'fname' => 'Lecturer',
+            'mname' => 'Lecturer Middlename',
+            'lname' => 'Lecturer Lastname',
+            'email' => 'lecturer@gmail.com',
+            'password' => bcrypt('lecturer'),
+        ]);
+
+        DB::table('users')->insert([
+            'fname' => 'Learner',
+            'mname' => 'Learner Middlename',
+            'lname' => 'Learner Lastname',
+            'email' => 'learner@gmail.com',
+            'password' => bcrypt('learner'),
+        ]);
     }
 }
