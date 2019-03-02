6a00b1edf97d796e24920abce5daff65c1320e8d
Remarc Balisi <remarcbalisi@gmail.com>
2019-03-02 10:51:39 +0800

    add new admin layout

--- resources/views/admin/course/courses_by_semester_ay.blade.php
+++ resources/views/admin/course/courses_by_semester_ay.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">{{$academic_year_semester->getSemesterFullName()}} Courses</div>
 
